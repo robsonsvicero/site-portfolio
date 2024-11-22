bdff64abf6c6b3589e3f883744b5ba2fa5ddc8d8
Robson Svicero <robsonsvicero@outlook.com>
2024-11-22 18:35:25 -0300

    Alterações no header e no footer

--- src/js/index.js
+++ src/js/index.js
@@ -1,17 +1,30 @@
+
 //Alterar propriedades menu hamburguer e logo
-const bkgHamburger = document.getElementById('hamburguer');
+const bkgHamburger = {
+    id1: document.getElementById('hamburger'),
+    id2: document.getElementById('hamburger2'),
+    id3: document.getElementById('hamburger3')
+};
 const bkgMenu = document.getElementById('bkg-menu');
 const logo = document.getElementById('logo');
 
+const menu = document.getElementById('menu-header');
+
 window.addEventListener("scroll", () => {
     if (window.scrollY > 639) {
-        bkgHamburger.classList.add("alterado");
+        bkgHamburger.id1.classList.add("alterado");
+        bkgHamburger.id2.classList.add("alterado");
+        bkgHamburger.id3.classList.add("alterado");
         bkgMenu.classList.add("fundo");
         logo.style.display = "none";
+        menu.style.justifyContent = "flex-end";
     } else {
-        bkgHamburger.classList.remove("alterado");
+        bkgHamburger.id1.classList.remove("alterado");
+        bkgHamburger.id2.classList.remove("alterado");
+        bkgHamburger.id3.classList.remove("alterado");
         bkgMenu.classList.remove("fundo");
         logo.style.display = "block";
+        menu.style.justifyContent = "space-between";
     }
 });
 
@@ -26,3 +39,8 @@ btn.onmousemove = function(e) {
     btn.style.setProperty('--eixoX', x + "px");
     btn.style.setProperty('--eixoY', y + "px");
 };
+
+// Função para fechar o menu ao clicar em um item
+function closeMenu() {
+    document.getElementById('menu-toggle').checked = false;
+  };
